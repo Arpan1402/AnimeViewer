f89f4968d49770d4dc71540f9fcbe2edcc327620
Arpan <arpandas1402@gmail.com>
2022-04-24 15:16:07 +0530

    Added cover in home

---
+++ src/app/components/shared/shared.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Card1Component } from './cards/card1/card1.component';
+
+
+
+@NgModule({
+  declarations: [
+    Card1Component
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
